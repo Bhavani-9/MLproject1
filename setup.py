8a61f19c5914c3140c2f861d6a9d3be73860f094
Bhavani-9 <bhavanibhisetti@gmail.com>
2025-01-02 14:44:30 +0530

    added files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_BOT = '-e .'
+HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path: str) -> List[str]:
     '''
@@ -12,8 +12,8 @@ def get_requirements(file_path: str) -> List[str]:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]  # Corrected line
 
-        if HYPEN_E_BOT in requirements:
-            requirements.remove(HYPEN_E_BOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     return requirements
 
 setup(
